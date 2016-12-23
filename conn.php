def503bcffffe62a5c66dd115addbcdcbf1ba8c4
Tenuun Khangaitan <tenuun.khangaitan@gmail.com>
2016-12-23 13:48:14 +0800

    connect to MySQL

--- conn.php
+++ conn.php
@@ -1,2 +1,13 @@
 <?php
-  $conn = new mysqli('http://phpstack-1850-4828-194340.cloudwaysapps.com', 'sznszdskgy', 'tn6C3EeE9v', 'sznszdskgy');
+  $servername = "phpstack-1850-4828-194340.cloudwaysapps.com";
+  $username = "sznszdskgy";
+  $password = "tn6C3EeE9v";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password);
+
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  echo "Connected successfully";
