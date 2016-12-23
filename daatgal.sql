4b263da97bdc118aaa7b522216ca28d18c6519d1
Tenuun Khangaitan <tenuun.khangaitan@gmail.com>
2016-12-23 17:45:49 +0800

    Daatgal pikchaas

---
+++ daatgal.sql
@@ -0,0 +1,108 @@
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Аръяасүрэн Батболд ','Гүйцэтгэх Захирал  (Ард-Даатгал)','АД 1.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Намдалдагва Эрдэнэтулга','Дэд захирал (Ард-Даатгал)','АД 2.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Тогтохбаяр Батцэнгэл','Санхүү Удирдлагын Газрын Захирал  (Ард-Даатгал)','АД 3.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Энхтайван  Жавхланбаяр','ДАА захирал (Ард-Даатгал)','АД 4.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Пүрэвсүрэн  Бямбасүрэн ','Дотоод аудитор (Ард-Даатгал)','АД 5.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Түвшинжаргал Мөнх-Оргил','Борлуулалтын Албаны дарга (Ард-Даатгал)','АД 6.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Болд Болортуяа','Тэргүүлэх менежер (Ард-Даатгал)','АД 7.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Дашдондов Баттулга','Даатгалын Ерөнхий менежер (Ард-Даатгал)','АД 8.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Дагвабалжир Эрдэнэчимэг','Даатгалын Ерөнхий менежер (Ард-Даатгал)','АД 9.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Отгон Майбаяр','Даатгалын менежер  (Ард-Даатгал)','АД 10.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Наянжин Ачит ','Даатгалын менежер  (Ард-Даатгал)','АД 11.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Давааням  Чадраабал ','Даатгалын менежер  (Ард-Даатгал)','АД 12.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Амартөр Тамир','Маркетингийн менежер (Ард-Даатгал)','АД 13.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Дарьсүрэн Одмаа','Бизнес Шинжилгээ Эрсдлийн Удирдлагын Албаны Дарга (Ард-Даатгал)','АД 14.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Баттулга  Нямдэлгэр','Бизнес Шинжилгээ Эрсдлийн Удирдлагын Албаны Ажилтан (Ард-Даатгал)','АД 15.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Цоодол Билгүүн','Бүртгэлийн ажилтан (Ард-Даатгал)','АД 16.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Цэрэнсодном  Батсуурь','Салбарын Удирдлагын Албаны Захирал (Ард-Даатгал)','АД 17.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Балдан Ариунаа','Хүний Нөөцийн ахлах менежер  (Ард-Даатгал)','АД 18.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Бямбабат Оюунхишиг','Хуульч (Ард-Даатгал)','АД 19.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Цагаанцоож  Сандагдорж','Жолооч  (Ард-Даатгал)','АД 20.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Насанбат  Бямбасүрэн ','IT инженер  (Ард-Даатгал)','АД 21.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Ганхүү Баярбат','Стратеги удирдлага төлөвлөлтийн албаны дарга (Ард-Даатгал)','АД 22.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Оюунтөгс  Шинэцэцэг','Ахлах нягтлан бодогч (Ард-Даатгал)','АД 23.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Цээсүрэн Бямбажав','Нягтлан бодогч (Ард-Даатгал)','АД 24.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Доржбал Буянхишиг','Нягтлан бодогч (Ард-Даатгал)','АД 25.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Цэдэнбал Ариунцэцэг','Нягтлан бодогч (Ард-Даатгал)','АД 26.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Лувсан Насанбуян','Нярав (Ард-Даатгал)','АД 27.jpg'
+);
