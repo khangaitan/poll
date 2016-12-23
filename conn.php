1d4c83da370e96801d4eeddd4ad1e33ea94113d6
Tenuun Khangaitan <tenuun.khangaitan@gmail.com>
2016-12-23 15:09:08 +0800

    oh noooo

--- conn.php
+++ conn.php
@@ -2,9 +2,10 @@
   $servername = "phpstack-1850-4828-194340.cloudwaysapps.com";
   $username = "sznszdskgy";
   $password = "tn6C3EeE9v";
+  $database = "sznszdskgy";
 
   // Create connection
-  $conn = new mysqli($servername, $username, $password);
+  $conn = new mysqli($servername, $username, $password, $database);
 
   // Check connection
   if ($conn->connect_error) {
