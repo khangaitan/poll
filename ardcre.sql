31d74d4983fd2d516a1c4a3b0d7be4df512349e8
Tenuun Khangaitan <tenuun.khangaitan@gmail.com>
2016-12-26 11:40:09 +0800

    zaza

---
+++ ardcre.sql
@@ -0,0 +1,128 @@
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Чулуун Ганхуяг','Гүйцэтгэх захирал (Ard-Holdings)','ah1.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Ариунаа Идэрбат ','Санхүү болон Хөрөнгө оруулалт эрхэлсэн захирал (Ard-Holdings)','ah2.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Өлзийсайхан Батсайхан','ТУЗ-ийн нарийн бичиг (Ard-Holdings)','ah3.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Жаргалан Алтангүний','Үйл ажиллагаа хариуцсан захирал (Ard-Holdings)','ah4.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Соронзонболд Халиунаа','Хууль тогтоомжийн нийцэл, эрсдэл хариуцсан захирал (Ard-Holdings)','ah5.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Ганхуяг Сэлэнгэ','Нягтлан бодогч (Ard-Holdings)','ah6.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Баярмаа Цэнгүүн','Захирлын туслах (Ard-Holdings)','ah7.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Нямаа Мөнхбат','Ахлах санхүүгийн шинжээч (Ard-Securities)','ah8.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Батбилэг Золбоо','Санхүүгийн шинжээч (Ard-Securities)','ah9.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Жаргалсайхан Золзаяа','Санхүүгийн шинжээч (Ard-Securities)','ah10.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Ганхуяг Билгүүн','Дадлагажигч Ард Securities','ah11.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Төмөрбаатар Оюун-Эрдэнэ ','Нягтлан бодогч (Ard-Holdings)','ah12.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Амар Майзул  ','Оффис менежер (Ard-Holdings)','ah13.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Эрхэмбаяр Алтантуяа','Гүйцэтгэх захирал (Ард-Актив)','aa1.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Бат-Эрдэнэ Алунгоо ','Нягтлан бодогч (Ард-Актив)','aa2.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Энхболд Цолмон','Зээлийн мэргэжилтэн (Ард-Актив)','aa3.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Билэгт Тамир','Гүйцэтгэх захирал (Хөрөнгө Оруулагч Үндэстэн ХК)','in1.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Сүхбаатар Ерөөлт','Маркетингийн менежер  (Хөрөнгө Оруулагч Үндэстэн ХК)','in2.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Цэнд-Аюуш Должин','Гүйцэтгэх захирал (Монголын Тэтгэвэрийн Нэрийн Данс)','mt1.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Ганбаатар Энхсаруул','Туслах нягтлан (Монголын Тэтгэвэрийн Нэрийн Данс)','mt2.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Чулуун Ганзориг','Гүйцэтгэх захирал (Ард-Кредит)','ac1.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Должинжав Отгонжаргал','Зээл хариуцсан захирал (Ард-Кредит)','ac2.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Зоригт Эрдэнэ-Амгалан','Бизнес хөгжлийн захирал (Ард-Кредит)','ac3.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Сумъяа Энхбаатар','Зээлийн мэргэжилтэн (Ард-Кредит)','ac4.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Даваажав Мөнх-Од','Зээлийн мэргэжилтэн (Ард-Кредит)','ac5.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Даваадорж Дархижав','Нягтлан бодогч (Ард-Кредит)','ac6.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Чинзориг Гэндэнпил','Нягтлан бодогч (Ард-Кредит)','ac7.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Ганбаатар Хандмаа','Зээлийн мэргэжилтэн (Ард-Кредит)','ac8.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Дашдорж Сувдмаа','Маркетингийн менежер  (Ард-Кредит)','ac9.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Батсодном Жамбал','Жолооч (Ард-Кредит)','ac10.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Цэдэн-Иш Энхтүвшин','Маркетингийн менежер  (Ард-Кредит)','ac11.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Баасанхүү Ананда','Бариста (Ard-Holdings)','ah99.jpg'
+);
