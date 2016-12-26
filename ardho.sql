166295ba6bad83b656506b7d44ab39180a02de38
Tenuun Khangaitan <tenuun.khangaitan@gmail.com>
2016-12-26 12:12:22 +0800

    zailoo

---
+++ ardho.sql
@@ -0,0 +1,24 @@
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Н. Ганхүү','Гүйцэтгэх захирал (Akela-Security)','ake1.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Базарсад','Хамгаалагч (Akela-Security)','ake2.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'А. Энхмэнд','Брокер (Ard-Securities)','as1.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'М. Мөнх-Эрдэнэ','Дотоод аудитор (Ard-Holdings)','ma2.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Дашдондог Энхбилэг','Ахлах брокер (Ard-Securities)','ma3.jpg'
+);
+INSERT INTO candidates ( name, title, img) 
+VALUES (
+'Н. Содонсуяа','Дадлагажигч (Ard-Securities)','ma1.jpg'
+);
