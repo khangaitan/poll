02c5860463bd9fabd1643c070365713e31d39086
Tenuun Khangaitan <tenuun.khangaitan@gmail.com>
2016-12-26 18:30:20 +0800

    haagdlaa

--- index.php
+++ index.php
@@ -12,9 +12,9 @@
     <img style="width: 100%;" src='images/cover.jpg'>
     </header>
     <ul class="tab">
-      <li><a href="javascript:void(0)" style="background: ;" class="tablinks" onclick="openCity(event, 'London')" id="defaultOpen">Өглөөний шувуухай (punctual)</a></li>
-      <li><a href="javascript:void(0)" style="background: ;" class="tablinks" onclick="openCity(event, 'Paris')">Тусч баавгай (helpful)</a></li>
-      <li><a href="javascript:void(0)" style="background: ;" class="tablinks" onclick="openCity(event, 'Tokyo')">Яг байгалиараа (cool)</a></li>
+      <li><a href="javascript:void(0)" style="background: ;" class="tablinks" onclick="openCity(event, 'ondon')" id="defaultOpen">Өглөөний шувуухай (punctual)</a></li>
+      <li><a href="javascript:void(0)" style="background: ;" class="tablinks" onclick="openCity(event, 'aris')">Тусч баавгай (helpful)</a></li>
+      <li><a href="javascript:void(0)" style="background: ;" class="tablinks" onclick="openCity(event, 'okyo')">Яг байгалиараа (cool)</a></li>
     </ul>
     <div class="main">
         <?php
